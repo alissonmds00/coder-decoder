486f62b4178ed5f366609653e1a4b8bd25850738
Alisson Matias <alissonmds00@gmaill..com>
2024-01-25 18:16:10 -0300

    style: solved some style issues

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 const newSentences = [];
+
+const input = document.querySelector("#texto");
+const criptografarButton = document.querySelector("#criptografar");
+const descriptografarButton = document.querySelector("#descriptografar");
+const copiarButton = document.querySelector("#copiar");
+
 function dictionaryContent() {
   const dictionaryContainer = document.querySelector(".dictionary-container");
+  if (newSentences.length) {
+    copiarButton.classList.remove("disabled");
+    document.querySelector(".dictionary-container").classList.add("item")
+  } else {
+    copiarButton.classList.add("disabled");
+  }
   const content =
     newSentences.length == 0
       ? `
@@ -16,13 +28,8 @@ function dictionaryContent() {
   dictionaryContainer.innerHTML = content;
 }
 
-
-const input = document.querySelector("#texto")
-const criptografarButton = document.querySelector("#criptografar")
-const descriptografarButton = document.querySelector("#descriptografar")
-
 function clickCriptografar() {
-  criptografar(input.value)
+  criptografar(input.value);
 }
 
 function criptografar(value) {
@@ -34,7 +41,7 @@ function criptografar(value) {
   value = value.replace(regex[4], "ufat");
   newSentences.push(value);
   dictionaryContent();
-  return value
+  return value;
 }
 
 dictionaryContent();
